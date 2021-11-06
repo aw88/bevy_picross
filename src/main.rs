74c060e48fe2048c801867a00808be9d1f0b8e3f
Alex Walton <alex.w.walton@gmail.com>
2021-11-06 19:03:47 +0000

    (temp) Initial game state implementation

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,30 @@ use bevy::prelude::*;
 
 mod board;
 
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum AppState {
+    MainMenu,
+    Puzzle,
+}
+
 struct MainCamera;
 struct UiCamera;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_plugin(board::BoardPlugin)
-        .add_startup_system(spawn_cameras.system())
+        .add_state(AppState::MainMenu)
+        .add_system_set(
+            SystemSet::on_enter(AppState::MainMenu)
+                .with_system(hello.system())
+        )
+        // .add_plugin(board::BoardPlugin)
+        // .add_startup_system(spawn_cameras.system())
         .run();
+}
+
+fn hello() {
+    print!("Hello");
 }
 
 fn spawn_cameras(mut commands: Commands) {
